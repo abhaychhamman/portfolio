b4c0c57cbd795a6a78f9bac9656ed44a9d937baa
abhaysingh <www.abhaysingh722@gmail.com>
2023-02-04 20:30:25 +0530

    added

--- src/component/Basic.js
+++ src/component/Basic.js
@@ -1,23 +1,21 @@
  
 import abhayImg from './../abhayImg.jpg';
 import abhayresume from './../abhay resume.pdf';
+
+
 export function Basic() {
+
+    
+
+
+    
     return (
       <>
 
       <nav>
         <div className="navleft item"><a href="/">Abhay Kuamr Singh</a></div> 
 
-        <div className="hamburger">MENU</div>
-        <div className="navright item">
-
-            <div className="home navlinks"><a href="home">home</a> </div>
-            <div className="about navlinks"><a href="about">about</a> </div>
-            <div className="Projects navlinks"><a href="Projects">Projects</a> </div>
-            <div className="Insta navlinks"><a href="Insta">Insta</a> </div>
-            <div className="Facebook navlinks"><a href="Facebook">Facebook</a> </div>
-            <div className="Youtube navlinks"><a href="Youtube">Youtube</a> </div>
-        </div>
+         
 
       </nav>
       
@@ -27,7 +25,7 @@ export function Basic() {
         <div className="detailsleft">
             <h1 className='smallfont'>I am a Full Stack </h1>
             <h1 className='largefont'>Web Developer </h1>
-            <p>my experties in main areas CSS,HTML ,JAVASCRIPT,PYTHON ,DJANGO ,REACT ,AJAX,JQUERY</p>
+            <p>my experties in main areas CSS,HTML ,JAVASCRIPT,PYTHON ,DJANGO  ,AJAX,JQUERY</p>
              </div> 
         <div className="detailsright">
             <img src={abhayImg} alt=""   />
